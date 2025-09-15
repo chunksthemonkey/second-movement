fdba93bf2cc235d2e1a261f710bf124df0c0dc5f
chunksthemonkey <58223156+chunksthemonkey@users.noreply.github.com>
2025-09-15 22:23:42 +0100

    Added TOTP keys and Periodic Table face

--- movement_config.h
+++ movement_config.h
@@ -29,16 +29,24 @@
 
 const watch_face_t watch_faces[] = {
     clock_face,
-    world_clock_face,
+    countdown_face,
+    stopwatch_face,
+    alarm_face,
+    probability_face,
     sunrise_sunset_face,
     moon_phase_face,
-    stopwatch_face,
-    countdown_face,
-    alarm_face,
+    totp_face,
+    periodic_table_face,
+
+    world_clock_face,
+    activity_logging_face,
+    accelerometer_status_face,
     temperature_display_face,
     voltage_face,
+    set_time_face,
     settings_face,
-    set_time_face
+    finetune_face,
+    nanosec_face,
 };
 
 #define MOVEMENT_NUM_FACES (sizeof(watch_faces) / sizeof(watch_face_t))
@@ -49,7 +57,7 @@ const watch_face_t watch_faces[] = {
  * Some folks also like to use this to hide the preferences and time set faces from the normal rotation.
  * If you don't want any faces to be excluded, set this to 0 and a long Mode press will have no effect.
  */
-#define MOVEMENT_SECONDARY_FACE_INDEX (MOVEMENT_NUM_FACES - 4)
+#define MOVEMENT_SECONDARY_FACE_INDEX (MOVEMENT_NUM_FACES - 9)
 
 /* Custom hourly chime tune. Check movement_custom_signal_tunes.h for options. */
 #define SIGNAL_TUNE_DEFAULT
